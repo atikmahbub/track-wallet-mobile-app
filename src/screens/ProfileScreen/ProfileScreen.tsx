91aa5895e5b3a3babb7e736eb006b18eb0350e4d
atikmahbub <atikmahbub100@gmail.com>
2024-12-01 17:42:46 +0600

    added all screens

--- src/screens/ProfileScreen/ProfileScreen.tsx
+++ src/screens/ProfileScreen/ProfileScreen.tsx
@@ -1,23 +1,77 @@
-import {View, Text} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import React from 'react';
 import {darkTheme} from '@trackingPortal/themes/darkTheme';
-import {Button} from 'react-native-paper';
+import {Avatar, Card, Text} from 'react-native-paper';
 import {useAuth} from '@trackingPortal/auth/Auth0ProviderWithHistory';
-import {AnimatedLoader} from '@trackingPortal/components';
+import {AnimatedLoader, ValueWithLabel} from '@trackingPortal/components';
+import {useStoreContext} from '@trackingPortal/contexts/StoreProvider';
+import {ScrollView} from 'react-native-gesture-handler';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 export default function ProfileScreen() {
   const {logout, loading} = useAuth();
+  const {currentUser: user} = useStoreContext();
 
   if (loading) {
     return <AnimatedLoader />;
   }
 
   return (
-    <View>
-      <Text style={[{color: darkTheme.colors.text}]}>InvestScreen</Text>
-      <Button mode="text" onPress={logout}>
-        Log out
-      </Button>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.avatarContainer}>
+        <Avatar.Image
+          size={100}
+          source={{
+            uri: user?.profilePicture ?? '',
+          }}
+        />
+      </View>
+      <Card style={styles.profileCard} mode="elevated">
+        <Card.Content>
+          <ValueWithLabel label="Name" value={user?.name || 'N/A'} />
+          <ValueWithLabel label="Email" value={user?.email || 'N/A'} />
+        </Card.Content>
+      </Card>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => {
+          logout();
+        }}>
+        <Icon name="logout" size={30} color={'white'} />
+        <Text style={styles.buttonText}>Log out</Text>
+      </TouchableOpacity>
+    </ScrollView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    backgroundColor: darkTheme.colors.background,
+  },
+  avatarContainer: {
+    paddingTop: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  profileCard: {
+    marginTop: 40,
+    backgroundColor: darkTheme.colors.surface,
+    padding: 10,
+  },
+  buttonContainer: {
+    marginTop: 20,
+    borderRadius: 10,
+    backgroundColor: darkTheme.colors.surface,
+    padding: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 10,
+  },
+  buttonText: {
+    color: darkTheme.colors.error,
+    fontSize: 18,
+    fontWeight: 700,
+  },
+});
