14d9d0871d3e38b7ec3bff65f8bd9ed378eabbfd
atikmahbub <atikmahbub100@gmail.com>
2024-12-02 23:08:42 +0600

    moda

--- src/components/FormModal.tsx
+++ src/components/FormModal.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  Keyboard,
+  TouchableWithoutFeedback,
+} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 import Modal from 'react-native-modal';
-import LoadingButton from './LoadingButton';
+import LoadingButton from '@trackingPortal/components/LoadingButton';
 
 interface IFormModal {
   isVisible: boolean;
@@ -20,22 +31,53 @@ const FormModal: React.FC<IFormModal> = ({
   loading,
   children,
 }) => {
+  const dismissKeyboard = () => Keyboard.dismiss();
+
   return (
     <Modal
       isVisible={isVisible}
-      onBackdropPress={onClose}
-      onBackButtonPress={onClose}
-      style={styles.modal}>
-      <View style={styles.modalContent}>
-        <Text style={styles.title}>{title}</Text>
-        {children}
-        <View style={styles.buttonContainer}>
-          <TouchableOpacity style={styles.cancelButton} onPress={onClose}>
-            <Text style={styles.buttonText}>Cancel</Text>
-          </TouchableOpacity>
-          <LoadingButton label="Save" loading={!!loading} onPress={onSave} />
-        </View>
-      </View>
+      onBackdropPress={() => {
+        dismissKeyboard();
+        onClose();
+      }}
+      onBackButtonPress={() => {
+        dismissKeyboard();
+        onClose();
+      }}
+      style={styles.modal}
+      animationIn="slideInUp"
+      animationOut="slideOutDown"
+      useNativeDriver
+      useNativeDriverForBackdrop
+      backdropTransitionOutTiming={0}
+      animationInTiming={300}
+      animationOutTiming={300}>
+      <SafeAreaView style={{flex: 1}}>
+        <KeyboardAvoidingView
+          behavior={Platform.OS === 'ios' ? 'padding' : undefined}
+          style={styles.keyboardAvoidingView}>
+          <TouchableWithoutFeedback onPress={dismissKeyboard}>
+            <View style={styles.modalContent}>
+              <Text style={styles.title}>{title}</Text>
+              <ScrollView
+                contentContainerStyle={styles.scrollView}
+                keyboardShouldPersistTaps="handled">
+                {children}
+              </ScrollView>
+              <View style={styles.buttonContainer}>
+                <TouchableOpacity style={styles.cancelButton} onPress={onClose}>
+                  <Text style={styles.buttonText}>Cancel</Text>
+                </TouchableOpacity>
+                <LoadingButton
+                  label="Save"
+                  loading={!!loading}
+                  onPress={onSave}
+                />
+              </View>
+            </View>
+          </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
     </Modal>
   );
 };
@@ -47,34 +89,38 @@ const styles = StyleSheet.create({
     justifyContent: 'flex-end',
     margin: 0,
   },
+  keyboardAvoidingView: {
+    flex: 1,
+    justifyContent: 'flex-end',
+  },
   modalContent: {
     backgroundColor: '#2A0845',
     borderTopLeftRadius: 20,
     borderTopRightRadius: 20,
-    padding: 20,
-    paddingBottom: 40,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+    paddingBottom: 50,
+    height: 'auto',
+    justifyContent: 'space-between',
   },
   title: {
     fontSize: 18,
     fontWeight: 'bold',
     color: '#E1BEE7',
-    paddingBottom: 25,
+    paddingBottom: 15,
   },
-  input: {
-    backgroundColor: '#3E206D',
-    color: '#E1BEE7',
-    padding: 10,
-    borderRadius: 5,
-    marginBottom: 10,
+  scrollView: {
+    flexGrow: 1,
   },
   buttonContainer: {
     flexDirection: 'row',
     justifyContent: 'flex-end',
-    marginTop: 20,
+    marginTop: 10, // Adjusted spacing above the buttons
   },
   cancelButton: {
     backgroundColor: '#7B1FA2',
-    padding: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
     borderRadius: 5,
     marginRight: 10,
   },
